c1ec04455b438c100ef4f9aa85077393a4592057
darmiel <71837281+darmiel@users.noreply.github.com>
2021-04-08 00:22:31 +0200

    Implemented

--- yaxc-ios/ViewController.swift
+++ yaxc-ios/ViewController.swift
@@ -17,26 +17,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func loadClipboardBtn(_ sender: Any) {
-        self.statusLbl.text = "clicked button."
-        
+    func getUrl() -> URL? {
         // get anywhere path
         let anywhere = self.anywherePathTxt.text
         if (anywhere == nil || anywhere!.isEmpty) {
             self.statusLbl.text = "No anywhere path set."
+            return nil
+        }
+        return URL(string: "https://yaxc.d2a.io/" + anywhere!)
+    }
+    
+    func msg(_ message: String) {
+        print(message)
+        DispatchQueue.main.async {
+            self.statusLbl.text = message
+        }
+    }
+    
+    @IBAction func loadClipboardBtn(_ sender: Any) {
+        // build url
+        let url = getUrl()
+        if url == nil {
             return
         }
-        
-        self.statusLbl.text = "nw resp."
+        print("lc:", url!)
         
         // make network request
-        let url = URL(string: "https://yaxc.d2a.io/" + anywhere!)!
         let session = URLSession.shared
-        
-        print(url)
-        
-        // read clipboard
-        session.dataTask(with: url, completionHandler: { data, response, error in
+        session.dataTask(with: url!, completionHandler: { data, response, error in
             if error != nil {
                 self.statusLbl.text = error!.localizedDescription
                 return
@@ -44,23 +52,59 @@ class ViewController: UIViewController {
             
             guard let httpResponse = response as? HTTPURLResponse,
                   (200...299).contains(httpResponse.statusCode) else {
-                print("status code not 2xx")
+                self.msg("status code not 2xx")
                 return
             }
             
             // read result
             let str = String(decoding: data!, as: UTF8.self)
-            print("Result: ", str)
+            self.msg(str)
             
             // write to clipboard
             UIPasteboard.general.string = str
-            
-            // write to status label
-            DispatchQueue.main.async {
-                self.statusLbl.text = str
-            }
         }).resume()
         
+    }
+    
+    @IBAction func writeClipboardBtn(_ sender: Any) {
+        // build url
+        let url = getUrl()
+        if url == nil {
+            return
+        }
+        print("u:", url!)
+        
+        // read clipboard
+        let cb = UIPasteboard.general.string
+        if cb == nil || cb!.isEmpty {
+            self.msg("Empty clipboard")
+            return
+        }
+        print("cb:", cb!)
+        
+        // make network request
+        let session = URLSession.shared
+        let data = Data(cb!.utf8)
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        
+        session.uploadTask(with: request, from: data) {data, response, error in
+            if error != nil {
+                self.msg("error: " + error!.localizedDescription)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                self.msg("status code not 2xx")
+                return
+            }
+            
+            // write result
+            let res = String(decoding: data!, as: UTF8.self)
+            self.msg(res)
+        }.resume()
     }
     
 }
